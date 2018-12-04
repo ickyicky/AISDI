4e775e4ee2d6f005b59769dff26645aa2405ed8b
ickyicky <35277436+ickyicky@users.noreply.github.com>
2018-12-04 11:19:02 +0100

    ok

--- Mapy/src/HashMap.h
+++ Mapy/src/HashMap.h
@@ -1,5 +1,6 @@
 #ifndef AISDI_MAPS_HASHMAP_H
 #define AISDI_MAPS_HASHMAP_H
+#define MAX_HASH 2047
 
 #include <cstddef>
 #include <initializer_list>
@@ -25,95 +26,229 @@ public:
   using iterator = Iterator;
   using const_iterator = ConstIterator;
 
+private:
+  class Node
+  {
+  public:
+    value_type data;
+    size_type hash;
+    Node* previous;
+    Node* next;
+    Node(value_type data, size_type hash, Node* previous=nullptr, Node* next=nullptr):
+          data(data), hash(hash), previous(previous), next(next)
+    {}
+  };
+
+  Node** buffer;
+  size_type size;
+  size_type biggest_hash;
+  size_type smallest_hash;
+  size_type max_hash;
+
+  size_type hashfun(key_type key) const
+  {
+    auto hash = std::hash<key_type>{}(key) % MAX_HASH;
+    return hash;
+  }
+
+  Node* ffind(key_type key) const
+  {
+    size_type hash = hashfun(key);
+    auto found = buffer[hash];
+    while(1)
+    {
+      if(found == nullptr) return nullptr;
+      if(found->data.first == key) break;
+      found = found->next;
+    }
+    return found;
+  }
+
+  Node* insert(key_type key)
+  {
+    value_type value = value_type(key, mapped_type{});
+    return insert(value);
+  }
+
+  Node* insert(value_type value)
+  {
+    size_type hash = hashfun(value.first);
+    if(hash > biggest_hash) biggest_hash = hash;
+    if(hash < smallest_hash) smallest_hash = hash;
+    Node* newnode = new Node(value,hash,nullptr,nullptr);
+    if(buffer[hash] != nullptr)
+    {
+      buffer[hash]->previous = newnode;
+      newnode->next = buffer[hash];
+    }
+    buffer[hash] = newnode;
+    size++;
+    return newnode;
+  }
+
+  void clear()
+  {
+    for(size_type i = 0; i < MAX_HASH; i++)
+    {
+        Node* current = buffer[i];
+        while(1)
+        {
+            if(current == nullptr) break;
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+        buffer[i] = nullptr;
+    }
+    size = biggest_hash = 0;
+    smallest_hash = MAX_HASH-1;
+  }
+
+public:
   HashMap()
-  {}
-
-  HashMap(std::initializer_list<value_type> list)
   {
-    (void)list; // disables "unused argument" warning, can be removed when method is implemented.
-    throw std::runtime_error("TODO");
+    size = 0;
+    biggest_hash = 0;
+    smallest_hash = MAX_HASH-1;
+    max_hash = MAX_HASH;
+    buffer = new Node*[max_hash];
+    for(size_type i =0;i<max_hash;i++)
+      buffer[i] = nullptr;
   }
 
-  HashMap(const HashMap& other)
+  HashMap(std::initializer_list<value_type> list): size(0), biggest_hash(0),smallest_hash(MAX_HASH-1), max_hash(MAX_HASH)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    buffer = new Node*[max_hash];
+    for(size_type i =0;i<max_hash;i++)
+      buffer[i] = nullptr;
+    for(auto i=list.begin(); i!=list.end(); i++ )
+      insert(*i);
   }
 
-  HashMap(HashMap&& other)
+  HashMap(const HashMap& other): size(0), biggest_hash(0),smallest_hash(MAX_HASH-1), max_hash(MAX_HASH)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    buffer = new Node*[max_hash];
+    for(size_type i =0;i<max_hash;i++)
+      buffer[i] = nullptr;
+    for(auto i=other.begin(); i!=other.end(); i++ )
+      insert(*i);
+  }
+
+  HashMap(HashMap&& other):
+  buffer(other.buffer), size(other.size), biggest_hash(other.biggest_hash),smallest_hash(other.smallest_hash), max_hash(other.max_hash)
+  {
+    other.buffer = nullptr;
+    other.size = other.max_hash = other.biggest_hash = other.smallest_hash = 0;
   }
 
   HashMap& operator=(const HashMap& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    if(&other == this) return *this;
+    clear();
+    for(auto i=other.begin(); i!=other.end(); i++ )
+      insert(*i);
+    return *this;
   }
 
   HashMap& operator=(HashMap&& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    if(&other != this)
+    {
+        clear();
+        buffer = other.buffer;
+        size= other.size;
+        biggest_hash = other.biggest_hash;
+        smallest_hash = other.smallest_hash;
+    }
+    other.buffer = nullptr;
+    other.size = other.max_hash = other.biggest_hash = other.smallest_hash = 0;
+
+    return *this;
   }
 
   bool isEmpty() const
   {
-    throw std::runtime_error("TODO");
+    return size==0;
   }
 
   mapped_type& operator[](const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    auto found = ffind(key);
+    if(found == nullptr)
+      found = insert(key);
+
+    return found->data.second;
   }
 
   const mapped_type& valueOf(const key_type& key) const
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    auto found = ffind(key);
+    if(found == nullptr)
+      throw std::out_of_range("out");
+
+    return found->data.second;
   }
 
   mapped_type& valueOf(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    auto found = ffind(key);
+    if(found == nullptr)
+      throw std::out_of_range("out");
+
+    return found->data.second;
   }
 
   const_iterator find(const key_type& key) const
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    return const_iterator(ffind(key), buffer);
   }
 
   iterator find(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    return iterator(ffind(key), buffer);
   }
 
   void remove(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    auto to_remove = ffind(key);
+    if(to_remove == nullptr)
+      throw std::out_of_range("out");
+
+    if(to_remove->previous == nullptr)
+      buffer[to_remove->hash] = to_remove->next;
+    else
+      to_remove->previous = to_remove->next;
+
+    if(to_remove->next != nullptr)
+      to_remove->next = to_remove->previous;
+
+    delete to_remove;
+    size--;
   }
 
   void remove(const const_iterator& it)
   {
-    (void)it;
-    throw std::runtime_error("TODO");
+    remove(it->first);
   }
 
   size_type getSize() const
   {
-    throw std::runtime_error("TODO");
+    return size;
   }
 
   bool operator==(const HashMap& other) const
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    auto i1 = begin();
+    auto i2 = other.begin();
+    while(1)
+    {
+      if(i1 != i2) return 0;
+      if((i1 == end()) && (i2 == other.end())) return 1;
+      if((i1 == end()) || (i2 == other.end())) return 0;
+      i1++;
+      i2++;
+    }
+    return 0;
   }
 
   bool operator!=(const HashMap& other) const
@@ -123,22 +258,24 @@ public:
 
   iterator begin()
   {
-    throw std::runtime_error("TODO");
+    if(isEmpty()) return end();
+    return iterator(buffer[smallest_hash],buffer);
   }
 
   iterator end()
   {
-    throw std::runtime_error("TODO");
+    return iterator(nullptr,buffer);
   }
 
   const_iterator cbegin() const
   {
-    throw std::runtime_error("TODO");
+    if(isEmpty()) return cend();
+    return const_iterator(buffer[smallest_hash],buffer) ;
   }
 
   const_iterator cend() const
   {
-    throw std::runtime_error("TODO");
+    return const_iterator(nullptr,buffer) ;
   }
 
   const_iterator begin() const
@@ -161,38 +298,100 @@ public:
   using value_type = typename HashMap::value_type;
   using pointer = const typename HashMap::value_type*;
 
-  explicit ConstIterator()
+private:
+  Node* node;
+  friend class HashMap<KeyType, ValueType>;
+  Node** buffer;
+
+public:
+
+  explicit ConstIterator(Node* node=nullptr, Node** buffer=nullptr): node(node), buffer(buffer)
   {}
 
   ConstIterator(const ConstIterator& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    node = other.node;
+    buffer = other.buffer;
   }
 
   ConstIterator& operator++()
   {
-    throw std::runtime_error("TODO");
+    if(node == nullptr)
+      throw std::out_of_range("out");
+
+    if(node->next == nullptr)
+    {
+      auto hash = node->hash + 1;
+      while(buffer[hash] == nullptr)
+      {
+        if(hash >= MAX_HASH-1)
+        {
+          node = nullptr;
+          return *this;
+        }
+        hash++;
+      }
+      node = buffer[hash];
+    }
+    else node = node->next;
+    return *this;
   }
 
   ConstIterator operator++(int)
   {
-    throw std::runtime_error("TODO");
+    auto old = ConstIterator(node, buffer);
+    operator++();
+    return old;
   }
 
   ConstIterator& operator--()
   {
-    throw std::runtime_error("TODO");
+    if(node == nullptr)
+    {
+      size_t i = MAX_HASH - 1;
+      while(i-- > 0)
+      {
+        if(buffer[i] == nullptr)
+          continue;
+        auto x = buffer[i];
+        while(x->next != nullptr)
+          x = x->next;
+        node = x;
+        return *this;
+      }
+      throw std::out_of_range("out");
+    }
+
+    if(node->previous == nullptr)
+    {
+      auto hash = node->hash - 1;
+      while(buffer[hash] == nullptr)
+      {
+        if(hash<=0)
+        {
+          hash = 0;
+          break;
+        }
+        hash--;
+      }
+      node = buffer[hash];
+    }
+    else node = node->previous;
+    return *this;
   }
 
   ConstIterator operator--(int)
   {
-    throw std::runtime_error("TODO");
+    auto old = ConstIterator(node, buffer);
+    operator--();
+    return old;
   }
 
   reference operator*() const
   {
-    throw std::runtime_error("TODO");
+    if(node == nullptr)
+      throw std::out_of_range("out_of_range");
+    return node->data;
   }
 
   pointer operator->() const
@@ -202,8 +401,16 @@ public:
 
   bool operator==(const ConstIterator& other) const
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    bool iif = 1;
+    if(node == nullptr || other.node == nullptr)
+    {
+      if(!(node == nullptr && other.node == nullptr))
+        iif = 0;
+    }
+    else
+      iif = node->data == other.node->data;
+
+    return iif;
   }
 
   bool operator!=(const ConstIterator& other) const
@@ -219,7 +426,7 @@ public:
   using reference = typename HashMap::reference;
   using pointer = typename HashMap::value_type*;
 
-  explicit Iterator()
+  explicit Iterator(Node* node=nullptr, Node** buffer=nullptr) : ConstIterator(node, buffer)
   {}
 
   Iterator(const ConstIterator& other)
--- Mapy/src/TreeMap.h
+++ Mapy/src/TreeMap.h
@@ -25,95 +25,249 @@ public:
   using iterator = Iterator;
   using const_iterator = ConstIterator;
 
-  TreeMap()
-  {}
-
-  TreeMap(std::initializer_list<value_type> list)
+private:
+  class node
   {
-    (void)list; // disables "unused argument" warning, can be removed when method is implemented.
-    throw std::runtime_error("TODO");
+  public:
+    value_type data;
+    node* left;
+    node* right;
+    node* parent;
+
+    node(value_type data, node* left=nullptr, node* right=nullptr, node* parent=nullptr):
+          data(data), left(left), right(right), parent(parent)
+    {}
+
+  };
+
+  node* root;
+  size_type size;
+  node* ffind(const value_type v)
+  {
+    if(isEmpty())
+    {
+      root = new node(v);
+      size++;
+      return root;
+    }
+
+    auto current = root;
+    while(current != nullptr)
+    {
+      auto backup = current;
+      if(current->data.first == v.first) break;
+      if(current->data.first > v.first)
+      {
+        current = current->left;
+        if(current == nullptr)
+        {
+          current = new node(v,nullptr,nullptr,backup);
+          backup->left = current;
+          size++;
+          break;
+        }
+      }
+      else
+      {
+        current = current->right;
+        if(current == nullptr)
+        {
+          current = new node(v,nullptr,nullptr,backup);
+          backup->right = current;
+          size++;
+          break;
+        }
+      }
+    }
+
+    return current;
   }
 
-  TreeMap(const TreeMap& other)
+  node* ffind(const key_type key)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    mapped_type map{};
+    value_type v(key,map);
+    return ffind(v);
+  }
+
+  node* ofind(const key_type key) const
+  {
+    auto current = root;
+    while(current != nullptr)
+    {
+      if(current->data.first == key) break;
+      if(current->data.first > key)
+        current = current->left;
+      else
+        current = current->right;
+    }
+    return current;
+  }
+
+  void balance(node* x)
+  {
+    (void) x;
+  }
+
+  node* minimum(node* x) const
+  {
+    if(isEmpty()) return nullptr;
+    while(x->left != nullptr)
+      x = x->left;
+    return x;
+  }
+
+  node* maximum(node* x) const
+  {
+    if(isEmpty()) return nullptr;
+    while(x->right != nullptr)
+      x = x->right;
+    return x;
+  }
+
+  void clear()
+  {
+    remover(root);
+    size = 0;
+    root = nullptr;
+  }
+
+  void remover(node* x)
+  {
+    if(x == nullptr) return;
+    remover(x->left);
+    remover(x->right);
+    delete x;
+  }
+
+public:
+
+  TreeMap(): root(nullptr), size(0)
+  {}
+
+  TreeMap(std::initializer_list<value_type> list): root(nullptr), size(0)
+  {
+    for(auto i = list.begin(); i != list.end(); i++)
+      ffind(*i);
+  }
+
+  TreeMap(const TreeMap& other): root(nullptr), size(0)
+  {
+    for(auto i = other.begin(); i != other.end(); i++)
+      ffind(*i);
   }
 
   TreeMap(TreeMap&& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    root = other.root;
+    other.root = nullptr;
+    size = other.size;
+    other.size = 0;
   }
 
   TreeMap& operator=(const TreeMap& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    clear();
+    for(auto i = other.begin(); i != other.end(); i++)
+      ffind(*i);
+    return *this;
   }
 
   TreeMap& operator=(TreeMap&& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    clear();
+    root = other.root;
+    other.root = nullptr;
+    size = other.size;
+    other.size = 0;
+    return *this;
   }
 
   bool isEmpty() const
   {
-    throw std::runtime_error("TODO");
+    return root==nullptr;
   }
 
   mapped_type& operator[](const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    auto found = ffind(key);
+    return found->data.second;
   }
 
   const mapped_type& valueOf(const key_type& key) const
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    return valueOf(key);
   }
 
   mapped_type& valueOf(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    auto found = ofind(key);
+    if(found == nullptr)
+      throw std::out_of_range("out_of_range");
+    return found->data.second;
   }
 
   const_iterator find(const key_type& key) const
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    return const_iterator(ofind(key));
   }
 
   iterator find(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    return iterator(ofind(key));
   }
 
   void remove(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    node* to_delete = ffind(key);
+    node* right = to_delete->right;
+    node* left = to_delete->left;
+
+    if(right != nullptr)
+    {
+      if(to_delete == root) root = right;
+      else if(to_delete->parent->right == to_delete) to_delete->parent->right = right;
+      else to_delete->parent->left = right;
+
+      if(left != nullptr)
+      {
+        maximum(left)->right = right->left;
+        right->left = left;
+        left->parent = right;
+        balance(right);
+      }
+    }
+    else
+    {
+      if(to_delete == root) root = left;
+      else if(to_delete->parent->right == to_delete) to_delete->parent->right = left;
+      else to_delete->parent->left = left;
+    }
   }
 
   void remove(const const_iterator& it)
   {
-    (void)it;
-    throw std::runtime_error("TODO");
+    remove(it.pointed->data.first);
   }
 
   size_type getSize() const
   {
-    throw std::runtime_error("TODO");
+    return size;
   }
 
   bool operator==(const TreeMap& other) const
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    auto i1 = begin();
+    auto i2 = other.begin();
+    while(1)
+    {
+      if(i1 != i2) return 0;
+      if((i1 == end()) && (i2 == other.end())) return 1;
+      if((i1 == end()) || (i2 == other.end())) return 0;
+      i1++;
+      i2++;
+    }
+    return 0;
   }
 
   bool operator!=(const TreeMap& other) const
@@ -123,23 +277,24 @@ public:
 
   iterator begin()
   {
-    throw std::runtime_error("TODO");
+    return iterator(minimum(root));
   }
 
   iterator end()
   {
-    throw std::runtime_error("TODO");
+    return iterator(nullptr,maximum(root));
   }
 
   const_iterator cbegin() const
   {
-    throw std::runtime_error("TODO");
+    return const_iterator(minimum(root));
   }
 
   const_iterator cend() const
   {
-    throw std::runtime_error("TODO");
+    return const_iterator(nullptr,maximum(root));
   }
+
 
   const_iterator begin() const
   {
@@ -155,44 +310,95 @@ public:
 template <typename KeyType, typename ValueType>
 class TreeMap<KeyType, ValueType>::ConstIterator
 {
+private:
+  friend class TreeMap<KeyType, ValueType>;
+  node* pointed;
+  node* maximal;
 public:
   using reference = typename TreeMap::const_reference;
   using iterator_category = std::bidirectional_iterator_tag;
   using value_type = typename TreeMap::value_type;
   using pointer = const typename TreeMap::value_type*;
 
-  explicit ConstIterator()
+  explicit ConstIterator(node* pointed, node* maximal=nullptr): pointed(pointed), maximal(maximal)
   {}
 
   ConstIterator(const ConstIterator& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    pointed = other.pointed;
   }
 
   ConstIterator& operator++()
   {
-    throw std::runtime_error("TODO");
+    if(pointed == nullptr)
+      throw std::out_of_range("out_of_range");
+    if(pointed->right != nullptr)
+    {
+      pointed = pointed->right;
+      while(pointed->right != nullptr)
+        pointed = pointed->right;
+      return *this;
+    }
+    while(pointed->parent != nullptr)
+    {
+      node* previous = pointed;
+      pointed = pointed->parent;
+      if(pointed->left == previous)
+        return *this;
+    }
+    pointed = nullptr;
+    return *this;
   }
 
   ConstIterator operator++(int)
   {
-    throw std::runtime_error("TODO");
+    auto old = ConstIterator(pointed,maximal);
+    operator++();
+    return old;
   }
 
   ConstIterator& operator--()
   {
-    throw std::runtime_error("TODO");
+  if(pointed == nullptr)
+  {
+      pointed = maximal;
+      return *this;
+  }
+
+  if(pointed->left != nullptr)
+  {
+    pointed = pointed->left;
+    while(pointed->left != nullptr)
+      pointed = pointed->left;
+    return *this;
+  }
+  while(pointed->parent != nullptr)
+  {
+    node* previous = pointed;
+    pointed = pointed->parent;
+    if(pointed->right == previous)
+      return *this;
+  }
+
+  throw std::out_of_range("out_of_range");
+
   }
 
   ConstIterator operator--(int)
   {
-    throw std::runtime_error("TODO");
+    auto old = ConstIterator(pointed,maximal);
+    operator--();
+    return old;
+    }
+
+    throw std::out_of_range("out_of_range");
   }
 
   reference operator*() const
   {
-    throw std::runtime_error("TODO");
+    if(pointed == nullptr)
+      throw std::out_of_range("out_of_range");
+    return pointed->data;
   }
 
   pointer operator->() const
@@ -202,8 +408,7 @@ public:
 
   bool operator==(const ConstIterator& other) const
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    return pointed == other.pointed;
   }
 
   bool operator!=(const ConstIterator& other) const
@@ -219,7 +424,7 @@ public:
   using reference = typename TreeMap::reference;
   using pointer = typename TreeMap::value_type*;
 
-  explicit Iterator()
+  explicit Iterator(node* pointed, node* maximal=nullptr): ConstIterator(pointed, maximal)
   {}
 
   Iterator(const ConstIterator& other)
